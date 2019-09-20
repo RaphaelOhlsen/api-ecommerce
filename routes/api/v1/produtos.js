5b602cfbbdbb360dcb3ee3d7f7a32d5ce78f31fa
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2019-09-20 08:00:55 -0300

    Criando as Rotas para o Modelo Produtos

--- routes/api/v1/produtos.js
+++ routes/api/v1/produtos.js
@@ -1,11 +1,30 @@
 const router = require('express').Router();
+
 import ProdutoController from '../../../controllers/ProdutoController';
 
 import auth from '../../auth';
 import Validation from 'express-validation';
 import { ProdutoValidation } from '../../../controllers/validacoes/produtovalidation';
+import { LojaValidation } from '../../../controllers/validacoes/lojavalidation';
+const upload = require('../../../config/multer');
 
 const produtoController = new ProdutoController();
+
+// ADMIN
+router.post('/', auth.required, LojaValidation.admin, produtoController.store);
+router.put('/:id', auth.required, LojaValidation.admin, produtoController.update);
+router.put('/images/:id', auth.required, LojaValidation.admin, upload.array('files', 4), produtoController.updateImages);
+router.delete('/:id', auth.required, LojaValidation.admin, produtoController.remove);
+
+// CLIENTE/VISITANTES
+router.get('/', produtoController.index);
+router.get('/disponiveis', produtoController.indexDisponiveis);
+router.get('/search/:search', produtoController.search);
+router.get('/:id', produtoController.show);
+
+// VARIACOES
+
+// AVALIACOES
 
 router.get('/',)
 

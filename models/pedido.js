3740f52c647c0d5c3cf5a3484d2944ff0cf09578
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2019-09-24 14:11:16 -0300

    Criando Modelo para o Modulo Pedidos

---
+++ models/pedido.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+import mongoosePaginate from 'mongoose-paginate';
+const Schema = mongoose.Schema;
+
+const PedidoSchema = Schema({
+  cliente: { type: Schema.Types.ObjectId, ref:"Cliente", required: true },
+  carrinho: { 
+    type: [{
+      produto: { type: Schema.Types.ObjectId, ref:"Produto", required: true },
+      variacao: { type: Schema.Types.ObjectId, ref:"Variacao", required: true },
+      produtoEstatico: { type: string }, 
+      quantidade: { type: Number, dafault: 1 },
+      precoUnitario: { type: Number, refired: true },
+    }]
+  },
+  pagamento: { type: Schema.Types.ObjectId, ref:"Pagamento", required: true },
+  entrega: { type: Schema.Types.ObjectId, ref: "Entrega", required: true},
+  cancelado: { type: Boolean, default: false },
+  loja: { type: Schema.Types.ObjectId, ref:"Loja", required: true }
+},{ timestamps: true });
+
+PedidoSchema.plugin(mongoosePaginate);
+
+module.exports = mongoose.model('Pedido', PedidoSchema);

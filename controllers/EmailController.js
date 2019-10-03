c3e031b5f8c7e69f730707a356d10ba269ad09fa
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2019-10-03 10:49:49 -0300

    Criando mensagens para Modificacoes por email

--- controllers/EmailController.js
+++ controllers/EmailController.js
@@ -26,17 +26,57 @@ const _send = ({ subject, emails, message }, cb = mull) => {
 
 // NOVO PEDIDO
 const enviarNovoPedido = ({ usuario, pedido }) => {
-
+  const message = `
+    <h1 style="text-align:center">Pedido Recebido</h1>
+    <br />
+    <p> O pedido realizado hoje, no dia ${momnet(pedido.createdAt).format('DD/MM/YYYY')}, foi recebido com sucesso.</p>
+    <br />
+    <a href="${loja}">Acesse a loja para saber mais.</a>
+    <p>Atenciosamente,</p>
+    <p>Equipe - Loja TI</p>
+  `;
+  _send({
+    subject: "Pedido Recebido - Loja TI",
+    emails: usuario.email,
+    message
+  })
 };
 
 // PEDIDO CANCELADO
 const cancelarPedido = ({ usuario, pedido }) => {
-
+  const message = `
+    <h1 style="text-align:center">Pedido Cancelado</h1>
+    <br />
+    <p> O pedido realizado no dia ${momnet(pedido.createdAt).format('DD/MM/YYYY')} foi cancelado.</p>
+    <br />
+    <a href="${loja}">Acesse a loja para saber mais.</a>
+    <p>Atenciosamente,</p>
+    <p>Equipe - Loja TI</p>
+  `;
+  _send({
+    subject: "Pedido Cancelado - Loja TI",
+    emails: usuario.email,
+    message
+  })
 };
 
 // ATUALIZAÇÃO DE PAGAMENTO E ENTREGA
 const atualizarPedido = ({ ususario, pedido, status, data, tipo }) => {
-
+  const message = `
+    <h1 style="text-align:center">Pedido Atualizado</h1>
+    <br />
+    <p> O pedido realizado no dia ${momnet(pedido.createdAt).format('DD/MM/YYYY')} sofreu uma atualização.</p>
+    <br />
+    <p>Nova Atualização: ${status} - realizado em ${moment(data).format('DD/MM/YYYY HH:mm')}</p>
+    <a href="${loja}">Acesse a loja para saber mais.</a>
+    <p>Atenciosamente,</p>
+    <p>Equipe - Loja TI</p>
+  `;
+  _send({
+    subject: "Pedido Atualizado - Loja TI",
+    emails: ususario.email,
+    message
+  })
 };
 
 module.exports = { 
